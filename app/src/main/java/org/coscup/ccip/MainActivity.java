a086477a6a0fdbf78655ba489197b32953d99b1b
Denny Huang <denny0223@gmail.com>
2016-06-26 01:29:18 +0800

    MainActivity get token from TokenUtil or Uri

--- app/src/main/java/org/coscup/ccip/MainActivity.java
+++ app/src/main/java/org/coscup/ccip/MainActivity.java
@@ -1,12 +1,14 @@
 package org.coscup.ccip;
 
-import android.net.Uri;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import org.coscup.ccip.model.Attendee;
 import org.coscup.ccip.network.CCIPClient;
+import org.coscup.ccip.util.TokenUtil;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -14,32 +16,46 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Activity mActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Uri uri = getIntent().getData();
-        if (uri != null) {
-            final TextView hello = (TextView) findViewById(R.id.hello);
-            final String token = uri.getQueryParameter("token");
+        final TextView hello = (TextView) findViewById(R.id.hello);
+        String token;
 
-            hello.setText(token);
-            Call<Attendee> attendee = CCIPClient.get().status(token);
-            attendee.enqueue(new Callback<Attendee>() {
-                @Override
-                public void onResponse(Call<Attendee> call, Response<Attendee> response) {
-                    if (response.isSuccessful()) {
-                        Attendee attendee = response.body();
-                        hello.setText(attendee.getUserId());
-                    }
-                }
+        mActivity = this;
 
-                @Override
-                public void onFailure(Call<Attendee> call, Throwable t) {
-                }
-            });
+        if (getIntent().getAction().equals(Intent.ACTION_VIEW)) {
+            token = getIntent().getData().getQueryParameter("token");
+            TokenUtil.setToken(mActivity, token);
+        } else {
+            token = TokenUtil.getToken(mActivity);
         }
 
+        if (token == null) {
+            hello.setText("Please open this app via link");
+            return;
+        }
+
+        Call<Attendee> attendee = CCIPClient.get().status(token);
+        attendee.enqueue(new Callback<Attendee>() {
+            @Override
+            public void onResponse(Call<Attendee> call, Response<Attendee> response) {
+                if (response.isSuccessful()) {
+                    Attendee attendee = response.body();
+                    hello.setText("Hello " + attendee.getUserId());
+                } else {
+                    hello.setText("invalid token");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Attendee> call, Throwable t) {
+                hello.setText("get status fail");
+            }
+        });
     }
 }
