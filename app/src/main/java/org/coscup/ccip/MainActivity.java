203cd4b1fc796c1adc2629921d88f1d2d767a76b
Denny Huang <denny0223@gmail.com>
2016-06-25 19:35:41 +0800

    Get user_id by token in MainActivity

--- app/src/main/java/org/coscup/ccip/MainActivity.java
+++ app/src/main/java/org/coscup/ccip/MainActivity.java
@@ -1,9 +1,16 @@
 package org.coscup.ccip;
 
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import org.coscup.ccip.model.Attendee;
+import org.coscup.ccip.network.CCIPClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
         Uri uri = getIntent().getData();
         if (uri != null) {
-            TextView hello = (TextView) findViewById(R.id.hello);
-            String token = uri.getQueryParameter("token");
+            final TextView hello = (TextView) findViewById(R.id.hello);
+            final String token = uri.getQueryParameter("token");
 
             hello.setText(token);
+            Call<Attendee> attendee = CCIPClient.get().status(token);
+            attendee.enqueue(new Callback<Attendee>() {
+                @Override
+                public void onResponse(Call<Attendee> call, Response<Attendee> response) {
+                    if (response.isSuccessful()) {
+                        Attendee attendee = response.body();
+                        hello.setText(attendee.getUserId());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Attendee> call, Throwable t) {
+                }
+            });
         }
 
     }
