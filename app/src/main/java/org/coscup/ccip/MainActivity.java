4d99baf862bd4adcf1b131d23953c4755db77d45
Denny Huang <denny0223@gmail.com>
2016-06-26 07:57:25 +0800

    MainActivity support swip to refresh

--- app/src/main/java/org/coscup/ccip/MainActivity.java
+++ app/src/main/java/org/coscup/ccip/MainActivity.java
@@ -3,6 +3,7 @@ package org.coscup.ccip;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,15 +22,20 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private Activity mActivity;
+    TextView hello;
+    RecyclerView scenarioView;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView hello = (TextView) findViewById(R.id.hello);
-        final RecyclerView scenarioView = (RecyclerView) findViewById(R.id.scenarios);
-        String token;
+        hello = (TextView) findViewById(R.id.hello);
+        scenarioView = (RecyclerView) findViewById(R.id.scenarios);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+
+        final String token;
 
         mActivity = this;
         scenarioView.setLayoutManager(new LinearLayoutManager(mActivity));
@@ -47,10 +53,23 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        updateStatus(token);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                updateStatus(token);
+            }
+        });
+
+    }
+
+    void updateStatus(String token) {
         Call<Attendee> attendee = CCIPClient.get().status(token);
         attendee.enqueue(new Callback<Attendee>() {
             @Override
             public void onResponse(Call<Attendee> call, Response<Attendee> response) {
+                swipeRefreshLayout.setRefreshing(false);
                 if (response.isSuccessful()) {
                     Attendee attendee = response.body();
                     hello.setText("Hello " + attendee.getUserId());
@@ -62,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Attendee> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
                 hello.setText("get status fail");
             }
         });
