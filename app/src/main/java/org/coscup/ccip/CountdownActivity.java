39ddc066c58005ab68521a354048ffa955c44d95
Denny Huang <denny0223@gmail.com>
2016-07-02 23:51:19 +0800

    Add empty activity CountdownActivity

---
+++ app/src/main/java/org/coscup/ccip/CountdownActivity.java
@@ -0,0 +1,13 @@
+package org.coscup.ccip;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CountdownActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countdown);
+    }
+}
