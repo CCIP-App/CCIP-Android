e5e1a19a9fb795f926042073eb8e28577118f85f
Denny Huang <denny0223@gmail.com>
2016-07-06 02:12:32 +0800

    Add ScheduleFragment

---
+++ app/src/main/java/org/coscup/ccip/fragment/ScheduleFragment.java
@@ -0,0 +1,22 @@
+package org.coscup.ccip.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.coscup.ccip.R;
+
+public class ScheduleFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        return view;
+    }
+
+}
