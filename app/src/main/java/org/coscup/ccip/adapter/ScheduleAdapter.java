71c90400bf3a64c0f65ad380ae4641666895a9b8
Denny Huang <denny0223@gmail.com>
2016-07-08 03:51:35 +0800

    Update item_schedule layout

--- app/src/main/java/org/coscup/ccip/adapter/ScheduleAdapter.java
+++ app/src/main/java/org/coscup/ccip/adapter/ScheduleAdapter.java
@@ -1,7 +1,6 @@
 package org.coscup.ccip.adapter;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -11,12 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-
-import org.coscup.ccip.CountdownActivity;
 import org.coscup.ccip.R;
 import org.coscup.ccip.model.Program;
-import org.coscup.ccip.model.Scenario;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -28,16 +23,11 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView scenarioName, status, allowTimeRange, disableReason;
-        public CardView card;
+        public TextView startTimeText;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            scenarioName = (TextView) itemView.findViewById(R.id.scenario_name);
-            status = (TextView) itemView.findViewById(R.id.status);
-            allowTimeRange = (TextView) itemView.findViewById(R.id.allow_time_range);
-            disableReason = (TextView) itemView.findViewById(R.id.disable_reason);
-            card = (CardView) itemView.findViewById(R.id.card);
+            startTimeText = (TextView) itemView.findViewById(R.id.start_time);
         }
     }
 
@@ -60,21 +50,7 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ViewHolder> {
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd HH:mm:ss");
 
         final List<Program> programs = mProgramSlotList.get(position);
-        holder.scenarioName.setText(programs.get(0).getStarttime());
-    }
-
-    public void startCountdownActivity(Scenario scenario) {
-        Gson gson = new Gson();
-        Intent intent = new Intent();
-        intent.setClass(mContext, CountdownActivity.class);
-        intent.putExtra(CountdownActivity.INTENT_EXTRA_SCENARIO, gson.toJson(scenario));
-        mContext.startActivity(intent);
-    }
-
-    private void setCardUnclickable(CardView card) {
-        card.setClickable(false);
-        card.setOnClickListener(null);
-        card.setCardBackgroundColor(Color.LTGRAY);
+        holder.startTimeText.setText(programs.get(0).getStarttime());
     }
 
     @Override
