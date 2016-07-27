8ac8aa43dd2d1a1ff48d7f47c8b1e98fe0d3e327
Denny Huang <denny0223@gmail.com>
2016-07-28 02:31:43 +0800

    Basic version of unused scenario

--- app/src/main/java/org/coscup/ccip/adapter/ScenarioAdapter.java
+++ app/src/main/java/org/coscup/ccip/adapter/ScenarioAdapter.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +14,7 @@ import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,15 +44,15 @@ public class ScenarioAdapter extends RecyclerView.Adapter<ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView scenarioName, status, allowTimeRange, disableReason;
+        public ImageView scenarioIcon;
+        public TextView scenarioName, allowTimeRange;
         public CardView card;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            scenarioIcon = (ImageView) itemView.findViewById(R.id.icon);
             scenarioName = (TextView) itemView.findViewById(R.id.scenario_name);
-            status = (TextView) itemView.findViewById(R.id.status);
             allowTimeRange = (TextView) itemView.findViewById(R.id.allow_time_range);
-            disableReason = (TextView) itemView.findViewById(R.id.disable_reason);
             card = (CardView) itemView.findViewById(R.id.card);
         }
     }
@@ -72,13 +76,15 @@ public class ScenarioAdapter extends RecyclerView.Adapter<ViewHolder> {
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd HH:mm:ss");
 
         final Scenario scenario = mScenarioList.get(position);
+        int iconResId = mContext.getResources().getIdentifier(scenario.getId().indexOf("lunch") > 0 ? "lunch" : scenario.getId(), "drawable", mContext.getPackageName());
+        holder.scenarioIcon.setImageDrawable(ContextCompat.getDrawable(mContext, iconResId));
         holder.scenarioName.setText(mContext.getResources().getIdentifier(scenario.getId(), "string", mContext.getPackageName()));
 
-        if (scenario.getUsed() == null) {
-            holder.status.setText("Unused");
-        } else {
-            holder.status.setText(sdf.format(new Date(scenario.getUsed() * 1000L)));
-        }
+//        if (scenario.getUsed() == null) {
+//            holder.status.setText("Unused");
+//        } else {
+//            holder.status.setText(sdf.format(new Date(scenario.getUsed() * 1000L)));
+//        }
 
         sdf = new SimpleDateFormat("MM/dd HH:mm");
         StringBuffer timeRange = new StringBuffer();
@@ -89,8 +95,6 @@ public class ScenarioAdapter extends RecyclerView.Adapter<ViewHolder> {
 
         if (scenario.getDisabled() != null) {
             holder.allowTimeRange.setVisibility(View.GONE);
-            holder.disableReason.setVisibility(View.VISIBLE);
-            holder.disableReason.setText(scenario.getDisabled());
             setCardUnclickable(holder.card);
             return;
         }
