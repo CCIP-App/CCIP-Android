0f2f795e1b5a1ca458911320d50a901dfd969b80
Denny Huang <denny0223@gmail.com>
2016-07-30 12:37:08 +0800

    PreferenceUtil add save programs and load programs

--- app/src/main/java/org/coscup/ccip/util/PreferenceUtil.java
+++ app/src/main/java/org/coscup/ccip/util/PreferenceUtil.java
@@ -3,6 +3,14 @@ package org.coscup.ccip.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.coscup.ccip.model.Program;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PreferenceUtil {
     private static final String PREF_AUTH = "auth";
     private static final String PREF_AUTH_TOKEN = "token";
@@ -20,5 +28,22 @@ public class PreferenceUtil {
     public static String getToken(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_AUTH, Context.MODE_PRIVATE);
         return sharedPreferences.getString(PREF_AUTH_TOKEN, null);
+    }
+
+    public static void savePrograms(Context context, List<Program> programs) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_SCHEDULE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        Gson gson = new Gson();
+        editor.putString(PREF_SCHEDULE_PROGRAMS, gson.toJson(programs));
+        editor.commit();
+    }
+
+    public static List<Program> loadPrograms(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_SCHEDULE, Context.MODE_PRIVATE);
+        String programsJson = sharedPreferences.getString(PREF_SCHEDULE_PROGRAMS, null);
+
+        Gson gson = new Gson();
+        return gson.fromJson(programsJson, new TypeToken<ArrayList<Program>>(){}.getType());
     }
 }
