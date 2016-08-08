89b49f0850b67c9da2313f53845ea081c0034c2f
Denny Huang <denny0223@gmail.com>
2016-08-09 00:30:32 +0800

    Let AnnouncementAdapter item background onclick has material effect

--- app/src/main/java/org/coscup/ccip/adapter/AnnouncementAdapter.java
+++ app/src/main/java/org/coscup/ccip/adapter/AnnouncementAdapter.java
@@ -3,6 +3,7 @@ package org.coscup.ccip.adapter;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,8 @@ import java.util.List;
 public class AnnouncementAdapter extends RecyclerView.Adapter<ViewHolder> {
 
     private Context mContext;
+    private final TypedValue mTypedValue = new TypedValue();
+    private int mBackground;
     private List<Announcement> announcementList;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -37,6 +40,9 @@ public class AnnouncementAdapter extends RecyclerView.Adapter<ViewHolder> {
     public AnnouncementAdapter(Context context, List<Announcement> programSlotList) {
         mContext = context;
         announcementList = programSlotList;
+
+        context.getTheme().resolveAttribute(R.attr.selectableItemBackground, mTypedValue, true);
+        mBackground = mTypedValue.resourceId;
     }
 
     @Override
@@ -44,6 +50,7 @@ public class AnnouncementAdapter extends RecyclerView.Adapter<ViewHolder> {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_announcement, parent, false);
 
+        itemView.setBackgroundResource(mBackground);
         return new ViewHolder(itemView);
     }
 
@@ -59,6 +66,12 @@ public class AnnouncementAdapter extends RecyclerView.Adapter<ViewHolder> {
             holder.msg.setText(announcement.getMsgEn());
         }
         holder.time.setText(sdf.format(new Date(announcement.getDatetime() * 1000L)));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
