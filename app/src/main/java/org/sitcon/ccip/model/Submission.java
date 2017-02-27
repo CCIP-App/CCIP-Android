044c07bc8d42ce6a1b738f4776246b0b0a99a61b
Denny Huang <denny0223@gmail.com>
2017-02-28 05:44:53 +0800

    Add SITCON submission models

---
+++ app/src/main/java/org/sitcon/ccip/model/Submission.java
@@ -0,0 +1,87 @@
+
+package org.sitcon.ccip.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Submission {
+
+    @SerializedName("speaker")
+    @Expose
+    private Speaker speaker;
+    @SerializedName("start")
+    @Expose
+    private String start;
+    @SerializedName("end")
+    @Expose
+    private String end;
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("room")
+    @Expose
+    private String room;
+    @SerializedName("subject")
+    @Expose
+    private String subject;
+    @SerializedName("summary")
+    @Expose
+    private String summary;
+
+    public Speaker getSpeaker() {
+        return speaker;
+    }
+
+    public void setSpeaker(Speaker speaker) {
+        this.speaker = speaker;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+}
