d22d37b4d4fe75f27dc0f77b7ad465eabab08065
Denny Huang <denny0223@gmail.com>
2017-03-13 23:17:53 +0800

    Remove fragment_irc, use fragment_web

--- app/src/main/java/org/sitcon/ccip/fragment/PuzzleFragment.java
+++ app/src/main/java/org/sitcon/ccip/fragment/PuzzleFragment.java
@@ -41,7 +41,7 @@ public class PuzzleFragment extends TrackFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_irc, container, false);
+        View view = inflater.inflate(R.layout.fragment_web, container, false);
 
         setHasOptionsMenu(true);
 
