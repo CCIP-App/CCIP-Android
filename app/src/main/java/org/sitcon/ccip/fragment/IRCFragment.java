fbce374e6e3d1612b6c50c4bf5889076aeac83b4
Denny Huang <denny0223@gmail.com>
2017-03-13 23:17:53 +0800

    Remove fragment_irc, use fragment_web

--- app/src/main/java/org/sitcon/ccip/fragment/IRCFragment.java
+++ app/src/main/java/org/sitcon/ccip/fragment/IRCFragment.java
@@ -30,7 +30,7 @@ public class IRCFragment extends TrackFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_irc, container, false);
+        View view = inflater.inflate(R.layout.fragment_web, container, false);
 
         progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
 
