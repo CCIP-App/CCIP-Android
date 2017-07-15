12a71327f73efceb08e60283198e54e0a95ef4bc
megshao <megshao0918@gmail.com>
2017-07-15 13:03:22 +0800

    Add ScheduleTabAdapter.

---
+++ app/src/main/java/org/coscup/ccip/adapter/ScheduleTabAdapter.java
@@ -0,0 +1,37 @@
+package org.coscup.ccip.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScheduleTabAdapter extends FragmentPagerAdapter {
+  private final List<Fragment> mFragmentList = new ArrayList<>();
+  private final List<String> mFragmentTitleList = new ArrayList<>();
+
+  public ScheduleTabAdapter(FragmentManager fm) {
+    super(fm);
+  }
+
+  @Override
+  public Fragment getItem(int position) {
+    return mFragmentList.get(position);
+  }
+
+  @Override
+  public int getCount() {
+    return mFragmentList.size();
+  }
+
+  @Override
+  public CharSequence getPageTitle(int position) {
+    return mFragmentTitleList.get(position);
+  }
+
+  public void addFragment(Fragment fragment, String title) {
+    mFragmentList.add(fragment);
+    mFragmentTitleList.add(title);
+  }
+
+}
