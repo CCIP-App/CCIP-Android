e23042d1f30320861b8c84b5b897a8cbe0c090f9
Denny Huang <denny0223@gmail.com>
2018-05-20 20:40:26 +0800

    Update PuzzleFragment url for PyConTW 2018

--- app/src/main/java/org/pycontw/ccip/fragment/PuzzleFragment.java
+++ app/src/main/java/org/pycontw/ccip/fragment/PuzzleFragment.java
@@ -31,7 +31,7 @@ import java.security.NoSuchAlgorithmException;
 public class PuzzleFragment extends Fragment {
 
     private static final String URL_NO_NETWORK = "file:///android_asset/no_network.html";
-    private static final String URL_PUZZLE = "https://king.sitcon.party/?mode=app&token=";
+    private static final String URL_PUZZLE = "https://game.pycon.tw/?mode=app&token=";
 
     private Activity mActivity;
 
