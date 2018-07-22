1d8dffb1313e20d275b4bfae542a41748f81cefa
yoyo930021 <yoyo930021@gmail.com>
2018-07-22 22:21:49 +0800

    Fix submission alarm accuracy

--- app/src/main/java/org/coscup/ccip/util/AlarmUtil.java
+++ app/src/main/java/org/coscup/ccip/util/AlarmUtil.java
@@ -4,13 +4,17 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
 import com.google.gson.internal.bind.util.ISO8601Utils;
+
+import org.coscup.ccip.activity.SubmissionDetailActivity;
+import org.coscup.ccip.model.Submission;
+
 import java.text.ParseException;
 import java.text.ParsePosition;
 import java.util.Calendar;
 import java.util.Date;
-import org.coscup.ccip.activity.SubmissionDetailActivity;
-import org.coscup.ccip.model.Submission;
 
 public class AlarmUtil {
 
@@ -27,7 +31,11 @@ public class AlarmUtil {
                     .getBroadcast(context, submission.hashCode(), intent, 0);
 
             AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis() - 10 * 60 * 1000, pendingIntent);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis() - 10 * 60 * 1000, pendingIntent);
+            else {
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis() - 10 * 60 * 1000, pendingIntent);
+            }
         } catch (ParseException e) {
             e.printStackTrace();
         }
