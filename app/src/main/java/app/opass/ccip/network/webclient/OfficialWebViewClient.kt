792d9de807dd4f2b9da648999c2067581bdcd496
james58899 <james59988@gmail.com>
2019-03-19 15:53:35 +0800

    Fix load error on SDK >= M

--- app/src/main/java/app/opass/ccip/network/webclient/OfficialWebViewClient.kt
+++ app/src/main/java/app/opass/ccip/network/webclient/OfficialWebViewClient.kt
@@ -1,16 +1,24 @@
 package app.opass.ccip.network.webclient
 
+import android.annotation.TargetApi
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class OfficialWebViewClient : WebViewClient() {
+    override fun onReceivedError(view: WebView, errorCode: Int, description: String?, failingUrl: String?) {
+        super.onReceivedError(view, errorCode, description, failingUrl)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) view.loadUrl("file:///android_asset/no_network.html")
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
     override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
         super.onReceivedError(view, request, error)
-        view.loadUrl("file:///android_asset/no_network.html")
+        if (request.isForMainFrame) view.loadUrl("file:///android_asset/no_network.html")
     }
 
     override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
