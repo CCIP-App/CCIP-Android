833884d92c83b98613dd547fdbe126aa33fd00e2
Coin <cce5784@gmail.com>
2019-07-22 22:32:52 +0800

    Move event resolving logic into AuthActivity

--- app/src/main/java/app/opass/ccip/activity/LauncherActivity.kt
+++ app/src/main/java/app/opass/ccip/activity/LauncherActivity.kt
@@ -2,18 +2,12 @@ package app.opass.ccip.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import app.opass.ccip.R
-import app.opass.ccip.extension.asyncExecute
 import app.opass.ccip.model.EventConfig
-import app.opass.ccip.network.CCIPClient
-import app.opass.ccip.network.PortalClient
 import app.opass.ccip.util.JsonUtil
 import app.opass.ccip.util.PreferenceUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 
 class LauncherActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,38 +20,25 @@ class LauncherActivity : AppCompatActivity(), CoroutineScope by MainScope() {
             t.printStackTrace()
             PreferenceUtil.setCurrentEvent(this, JsonUtil.fromJson("{\"event_id\": \"\"}", EventConfig::class.java))
         }
+        val currentEventId = PreferenceUtil.getCurrentEvent(this).eventId
+        if (intent.action == Intent.ACTION_VIEW) {
+            val eventId = intent.data!!.getQueryParameter("event_id") ?: ""
+            val token = intent.data!!.getQueryParameter("token") ?: ""
 
-        launch {
-            this@LauncherActivity.run {
-                if (intent.action == Intent.ACTION_VIEW) {
-                    val eventId = intent.data!!.getQueryParameter("event_id") ?: ""
-                    val token = intent.data!!.getQueryParameter("token") ?: ""
-
-                    if (eventId.isNotEmpty() && PreferenceUtil.getCurrentEvent(this).eventId != eventId) {
-                        try {
-                            val response = PortalClient.get().getEventConfig(eventId).asyncExecute()
-                            if (response.isSuccessful) {
-                                val eventConfig = response.body()!!
-                                PreferenceUtil.setCurrentEvent(this, eventConfig)
-                                CCIPClient.setBaseUrl(eventConfig.serverBaseUrl)
-                            }
-                        } catch (t: Throwable) {
-                            t.printStackTrace()
-                            Toast.makeText(this, R.string.offline, Toast.LENGTH_LONG).show()
-                        }
-                    }
-                    if (token.isNotEmpty()) {
-                        PreferenceUtil.setIsNewToken(this, true)
-                        PreferenceUtil.setToken(this, token)
-                    }
-                }
-                if (PreferenceUtil.getCurrentEvent(this).eventId.isNotEmpty()) {
-                    startActivity(Intent(this, MainActivity::class.java))
+            if (eventId.isNotEmpty()) {
+                if (token.isNotEmpty()) {
+                    startActivity(AuthActivity.createIntent(this, eventId, token))
                 } else {
-                    startActivity(Intent(this, EventActivity::class.java))
+                    startActivity(AuthActivity.createIntent(this, eventId, null))
                 }
-                finish()
+                return finish()
             }
         }
+        if (currentEventId.isNotEmpty()) {
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            startActivity(Intent(this, EventActivity::class.java))
+        }
+        finish()
     }
 }
