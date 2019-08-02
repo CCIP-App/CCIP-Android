3c78ad799e70a40683630785188f55b41b1b1708
Coin <cce5784@gmail.com>
2019-08-02 21:37:52 +0800

    MyTicketFragment: add empty view

--- app/src/main/java/app/opass/ccip/fragment/MyTicketFragment.kt
+++ app/src/main/java/app/opass/ccip/fragment/MyTicketFragment.kt
@@ -1,20 +1,23 @@
 package app.opass.ccip.fragment
 
-import android.app.Activity
+import android.content.Intent
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import app.opass.ccip.R
+import app.opass.ccip.activity.AuthActivity
 import app.opass.ccip.util.PreferenceUtil
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
 import com.google.zxing.common.BitMatrix
+import kotlinx.android.synthetic.main.fragment_my_ticket.*
+import kotlinx.android.synthetic.main.include_login.*
 
 class MyTicketFragment : Fragment() {
     companion object {
@@ -53,23 +56,41 @@ class MyTicketFragment : Fragment() {
         }
     }
 
-    private lateinit var mActivity: Activity
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        val view = inflater.inflate(R.layout.fragment_my_ticket, container, false)
+        return inflater.inflate(R.layout.fragment_my_ticket, container, false)
+    }
 
-        mActivity = requireActivity()
+    private fun updateView() {
+        val activity = requireActivity()
 
-        val qrcodeImageView = view.findViewById<ImageView>(R.id.qrcodeImage)
-
-        if (PreferenceUtil.getToken(mActivity) != null) {
+        if (PreferenceUtil.getToken(activity) != null) {
             val widthPixels = Resources.getSystem().displayMetrics.widthPixels / 4
 
-            val bm = encodeAsBitmap(PreferenceUtil.getToken(mActivity), BarcodeFormat.QR_CODE, widthPixels, widthPixels)
-            qrcodeImageView.setImageBitmap(bm)
-        }
+            val bm = encodeAsBitmap(PreferenceUtil.getToken(activity), BarcodeFormat.QR_CODE, widthPixels, widthPixels)
+            qrcodeImage.setImageBitmap(bm)
 
-        return view
+            qrcodeImage.isGone = false
+            ticket_notice.isGone = false
+            login_view.isGone = true
+        } else {
+            qrcodeImage.isGone = true
+            ticket_notice.isGone = true
+            login_view.isGone = false
+
+            login_button.setOnClickListener {
+                activity.startActivity(Intent(activity, AuthActivity::class.java))
+            }
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        updateView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateView()
     }
 }
