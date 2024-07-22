b8a53a496432bf9fefb35b50a9c8fbf422f7830b
Aayush Gupta <aayushgupta219@gmail.com>
2024-07-22 18:32:32 +0700

    Migrate to gradle build version catalogs
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.google.play.services) apply false
 }
