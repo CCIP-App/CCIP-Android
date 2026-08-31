36ef95b17cfcc2ad6ca192b891045298cb5e4b4b
Denny Huang <denny0223@gmail.com>
2026-08-31 14:10:30 -0700

    fix: handle unsupported WebView intents

--- app/src/main/java/app/opass/ccip/network/webclient/OfficialWebViewClient.kt
+++ app/src/main/java/app/opass/ccip/network/webclient/OfficialWebViewClient.kt
@@ -1,5 +1,6 @@
 package app.opass.ccip.network.webclient
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.webkit.WebResourceError
@@ -16,7 +17,24 @@ class OfficialWebViewClient : WebViewClient() {
 
     @Deprecated("Deprecated in Java")
     override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-        view.context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+        val intent = try {
+            if (url.startsWith("intent:")) Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+            else Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        } catch (_: java.net.URISyntaxException) {
+            return true
+        }
+        intent.addCategory(Intent.CATEGORY_BROWSABLE)
+        intent.component = null
+        intent.selector = null
+
+        try {
+            view.context.startActivity(intent)
+        } catch (_: ActivityNotFoundException) {
+            val fallbackUrl = intent.getStringExtra("browser_fallback_url") ?: return true
+            when (Uri.parse(fallbackUrl).scheme) {
+                "http", "https" -> view.loadUrl(fallbackUrl)
+            }
+        }
         return true
     }
 }
